40ae183e3956b8942f4503d4f5868a7b336312d9
MuhammedErdin <erdinmuhammed835@gmail.com>
2024-02-13 17:55:44 +0300

    Lesson 2

--- Intro/Business/CourseManager.cs
+++ Intro/Business/CourseManager.cs
@@ -1,4 +1,6 @@
-﻿using Intro.Entities;
+﻿using Intro.DataAccess.Abstract;
+using Intro.DataAccess.Concrete;
+using Intro.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,40 +11,15 @@ namespace Intro.Business
 {
     public class CourseManager
     {
-        Course[] courses = new Course[3];
-        //constructor
-        public CourseManager()
+        private readonly ICourseDal _courseDal;
+
+        public CourseManager(ICourseDal courseDal)
         {
-            {
-                Course course1 = new Course();
-                course1.Id = 1;
-                course1.Name = "C#";
-                course1.Description = ".NET 8 vs...";
-                course1.Price = 0;
-
-                Course course2 = new Course();
-                course2.Id = 2;
-                course2.Name = "Java";
-                course2.Description = "Java 17 ....";
-                course2.Price = 10;
-
-                Course course3 = new Course();
-                course3.Id = 3;
-                course3.Name = "Python";
-                course3.Description = "Python 3.12 ...";
-                course3.Price = 20;
-                Console.WriteLine("Başladı...");
-
-                courses[0] = course1;
-                courses[1] = course2;
-                courses[2] = course3;
-
-            }
+            _courseDal = courseDal;
         }
-
-        public Course[] GetAll()
+        public List<Course> GetAll()
         {
-            return courses;
+            return _courseDal.GetAll();
         }
     }
 }
--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Intro.Business;
+using Intro.DataAccess.Concrete;
 using Intro.Entities;
 using System;
 using System.Collections.Generic;
@@ -62,14 +63,48 @@ namespace Intro
             //    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
             //}
 
-            CourseManager courseManager = new CourseManager();
-            Course[] courses2=courseManager.GetAll();
-            for (int i = 0; i < courses2.Length; i++)
+            CourseManager courseManager = new CourseManager(new EfCourseDal());
+            List<Course> courses2=courseManager.GetAll();
+            for (int i = 0; i < courses2.Count; i++)
             {
                 Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
             }
 
             Console.WriteLine("Kod Bitti");
+
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.NationalIdentity = "12345678910";
+            customer1.FirstName = "Aslı";
+            customer1.LastName = "Karayiğit";
+            customer1.CustomerNumber = "123456";
+
+            IndividualCustomer customer2 = new IndividualCustomer();
+            customer2.Id = 2;
+            customer2.NationalIdentity = "12345678912";
+            customer2.FirstName = "Özgür";
+            customer2.LastName = "Atılgan";
+            customer2.CustomerNumber = "123457";
+
+            CoorporateCustomer customer3 = new CoorporateCustomer();
+            customer3.Id = 3;
+            customer3.Name = "Kodlama.io";
+            customer3.CustomerNumber = "654778";
+            customer3.TaxNumber = "12345678987";
+
+            CoorporateCustomer customer4 = new CoorporateCustomer();
+            customer4.Id = 4;
+            customer4.Name = "Abc";
+            customer4.CustomerNumber = "654779";
+            customer4.TaxNumber = "12345678985";
+
+
+            BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.CustomerNumber);
+            }
         }
     }
 }
